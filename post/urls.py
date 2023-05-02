73d804d798066f5ece91234ffa110248676c860e
preciousimo <preciousimoniakemu@gmail.com>
2023-05-02 22:42:47 +0100

    Create text posts

--- post/urls.py
+++ post/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('create/', views.post_create, name='post_create'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
-from account.models import User
-
+from .forms import PostForm
 from .models import Post
 from .serializers import PostSerializer
 
@@ -13,8 +11,24 @@ from .serializers import PostSerializer
 def post_list(request):
     posts = Post.objects.all()
 
+
     serializer = PostSerializer(posts, many=True)
 
     return JsonResponse(serializer.data, safe=False)
 
 
+@api_view(['POST'])
+def post_create(request):
+    form = PostForm(request.data)
+
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.created_by = request.user
+        post.save()
+
+        serializer = PostSerializer(post)
+
+        return JsonResponse(serializer.data, safe=False)
+    else:
+        return JsonResponse({'error': 'add somehting here later!...'})
+    
