4918e7b1f7d10eeb0ce03f0142b95a3f59d548ea
preciousimo <preciousimoniakemu@gmail.com>
2023-05-02 19:53:54 +0100

    Revamp code

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from . import api
+from . import views
 
 urlpatterns = [
-    path('me/', api.me, name='me'),
-    path('signup/', api.signup, name='signup'),
+    path('me/', views.me, name='me'),
+    path('signup/', views.signup, name='signup'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
