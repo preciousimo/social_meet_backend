188995c3316148782eaa4f2e917dab92b17279ef
preciousimo <preciousimoniakemu@gmail.com>
2023-05-02 20:47:19 +0100

    Create test feed

--- post/serializers.py
+++ post/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
+from account.serializers import UserSerializer
+
 from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    created_by = UserSerializer(read_only=True)
     class Meta:
         model = Post
         fields = ('id', 'body', 'likes_count', 'created_by', 'created_at_formatted',)
