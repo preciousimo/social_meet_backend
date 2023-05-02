804a90332af931e36884b36cd8a099d38dd4fcf0
preciousimo <preciousimoniakemu@gmail.com>
2023-05-02 19:46:30 +0100

    Create database model and register in django admin

--- post/models.py
+++ post/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.timesince import timesince
+
+from account.models import User
+
+
+class Like(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_by = models.ForeignKey(User, related_name='likes', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class PostAttachment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    image = models.ImageField(upload_to='post_attachments')
+    created_by = models.ForeignKey(User, related_name='post_attachments', on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    body = models.TextField(blank=True, null=True)
+
+    attachments = models.ManyToManyField(PostAttachment, blank=True)
+
+    likes = models.ManyToManyField(Like, blank=True)
+    likes_count = models.IntegerField(default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-created_at',)
+    
+    def created_at_formatted(self):
+       return timesince(self.created_at)
