1d7b1377b03af39ca09a723817fbe1bb3efd83ce
preciousimo <preciousimoniakemu@gmail.com>
2023-05-02 20:00:10 +0100

    Create views and urls for posts

--- post/views.py
+++ post/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+
+from account.models import User
+
+from .models import Post
+from .serializers import PostSerializer
+
+
+@api_view(['GET'])
+def post_list(request):
+    posts = Post.objects.all()
+
+    serializer = PostSerializer(posts, many=True)
+
+    return JsonResponse(serializer.data, safe=False)
+
+
--- social_meet_backend/urls.py
+++ social_meet_backend/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/', include(('account.urls', 'account'), namespace='account')),
+    path('api/', include('account.urls')),
+    path('api/posts/', include('post.urls')),
     path('admin/', admin.site.urls),
 ]
