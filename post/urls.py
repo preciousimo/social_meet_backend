bea6bd504e17037ba266afcafe7e2654faf0d636
preciousimo <preciousimoniakemu@gmail.com>
2023-05-23 18:31:09 +0100

    Liking posts

--- post/urls.py
+++ post/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('<uuid:pk>/like/', views.post_like, name='post_like'),
     path('profile/<uuid:id>/', views.post_list_profile, name='post_list_profile'),
     path('create/', views.post_create, name='post_create'),
 ]
