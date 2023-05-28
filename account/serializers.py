2cfdb7ca9d7e5599e83f804b778c1ad348b5cfd8
preciousimo <preciousimoniakemu@gmail.com>
2023-05-28 16:49:05 +0100

    Update

--- account/serializers.py
+++ account/serializers.py
@@ -4,14 +4,10 @@ from .models import User, FriendshipRequest
 
 
 class UserSerializer(serializers.ModelSerializer):
-    id = serializers.UUIDField(read_only=True, format='hex')
-    date_joined = serializers.DateTimeField(read_only=True)
-    last_login = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'name', 'email', 'friends_count', 'posts_count', 'get_avatar', 'is_active', 'date_joined', 'last_login')
-        read_only_field = ['is_active']
+        fields = ('id', 'name', 'email', 'friends_count', 'posts_count', 'get_avatar')
 
 
 class FriendshipRequestSerializer(serializers.ModelSerializer):
